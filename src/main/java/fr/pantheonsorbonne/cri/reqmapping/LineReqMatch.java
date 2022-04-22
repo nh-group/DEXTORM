abd06aebea4b3ae065648b7abb68521fc73fc7d4
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2022-04-22 11:58:52 +0200

    removed sysout

--- src/main/java/fr/pantheonsorbonne/cri/reqmapping/LineReqMatch.java
+++ src/main/java/fr/pantheonsorbonne/cri/reqmapping/LineReqMatch.java
@@ -17,7 +17,6 @@ public class LineReqMatch extends MethodReqMatch {
     public boolean isMatch(StackTraceElement elt) {
         if (this.getFQClassName().equals(elt.getClassName().replaceAll("/", "."))) {
             if (elt.getLineNumber() == this.getLine()) {
-                System.out.println("Match!");
                 return this.getReq().stream().anyMatch(Predicate.not(Strings::isNullOrEmpty));
             }
 
