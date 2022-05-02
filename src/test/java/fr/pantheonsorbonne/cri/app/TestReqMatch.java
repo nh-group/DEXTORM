1c3b6ce67d46b7b5eea0a6f9c0789826245e51c7
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2022-05-02 13:11:48 +0200

    more complete reqmatch test

--- src/test/java/fr/pantheonsorbonne/cri/app/TestReqMatch.java
+++ src/test/java/fr/pantheonsorbonne/cri/app/TestReqMatch.java
@@ -11,9 +11,38 @@ public class TestReqMatch {
     @Test
     void test() {
 
-        StackTraceElement ste = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;)i'", 10);
-        ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").args("([LString;)i").methodName("main").line(10).commit("1").build();
-        Assertions.assertTrue(rmatch.isMatch(ste));
+
+        {
+            StackTraceElement ste = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;)i'", 10);
+            ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").args("([LString;)i").methodName("main").line(10).commit("1").build();
+            Assertions.assertTrue(rmatch.isMatch(ste));
+        }
+
+        {
+            StackTraceElement ste = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;)i'", 10);
+            ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").args("[LString").methodName("main").line(10).commit("1").build();
+            Assertions.assertTrue(rmatch.isMatch(ste));
+        }
+
+        {
+            StackTraceElement ste = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;D;I;)V'", 10);
+            ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").arg("[LString").arg("D").arg("I").methodName("main").line(10).commit("1").build();
+            Assertions.assertTrue(rmatch.isMatch(ste));
+        }
+
+        {
+            StackTraceElement ste = new StackTraceElement("A.java", "toto", "toto.A", "main2", "([LString;D;I;)V'", 10);
+            ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").arg("[LString").arg("D").arg("I").methodName("main").line(10).commit("1").build();
+            Assertions.assertFalse(rmatch.isMatch(ste));
+        }
+
+        {
+            StackTraceElement ste1 = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;D;J;)V'", 10);
+            StackTraceElement ste2 = new StackTraceElement("A.java", "toto", "toto.A", "main", "([LString;D;D;)V'", 10);
+            ReqMatch rmatch = new ReqMatcherBuilder().className("A").packageName("toto").arg("[LString").arg("D").arg("I").methodName("main").line(10).commit("1").build();
+            Assertions.assertFalse(rmatch.isMatch(ste1));
+            Assertions.assertFalse(rmatch.isMatch(ste2));
+        }
 
 
     }
